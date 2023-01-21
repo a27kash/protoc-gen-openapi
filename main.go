42d8fe3c8a211ceb169c739c432109635a745ba3
a27kash <a27kash@gmail.com>
2023-01-21 21:26:03 +0530

    fill in openapi version number in document

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"google.golang.org/protobuf/compiler/protogen"
+	"gopkg.in/yaml.v3"
 )
+
+type Document struct {
+	Openapi	string	`yaml:"openapi"`
+}
 
 func main() {
 	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
+		d := Document{}
+		d.Openapi = "3.1.0"
+		bytes, err := yaml.Marshal(d)
+		if err != nil {
+			return fmt.Errorf("failed to marshal yaml: %s", err.Error())
+		}
 		outputFile := gen.NewGeneratedFile("openapi.yaml", "")
-		outputFile.Write(nil)
+		outputFile.Write(bytes)
 		return nil
 	})
 }
